94d2d9b0cce9a183d9059c3bdfe44d0abf7c24ba
jaxteller2016 <sorin.craciunescu84@gmail.com>
2020-05-03 18:26:31 +0300

    adding custom button and form input component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/Header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 
 function App() {
@@ -13,9 +15,10 @@ function App() {
       <Switch>
         <Route exact path= '/' component={HomePage}/>
         <Route exact path= '/shop' component={ShopPage}/>
+        <Route exact path= '/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
-}
+}   
 
 export default App;
